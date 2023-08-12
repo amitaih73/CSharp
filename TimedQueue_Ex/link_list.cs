8f30f8efc2ea83a5c442c83dd281a0c9f6b275af
Amitai <Amitai@AMITAI-LT>
2023-08-13 01:16:10 +0300

    Time queue times not finiushed but works fine

---
+++ TimedQueue_Ex/link_list.cs
@@ -0,0 +1,94 @@
+﻿using System.Diagnostics;
+
+namespace timedQueue_Ex_
+{
+    class Node
+    {
+        private readonly string m_message;
+        private readonly int m_when;
+
+        public Node(string a_message, int a_when)
+        {
+            m_message = a_message;
+            m_when = a_when;
+        }
+
+        public string Message()
+        {
+            return m_message;
+        }
+
+        public int DelayTime()
+        {
+            return m_when;
+        }
+    }
+
+   
+
+    class ListElements {
+        public ListElements Next { get; set; }
+        public Node Data { get; set; }
+        public ListElements(Node a_node)
+        {
+            Next = null;
+           Data = a_node;
+        }
+    }
+    class TimeQueue
+    {
+        private ListElements Last(ListElements a_element, ref ListElements a_oneBeforLast)
+        {
+            if (a_element == null) {
+                return new ListElements(new Node("empty!!!!", 0));
+            }
+            ListElements last = a_element;
+            while (last.Next != null)
+            {
+                a_oneBeforLast = last;
+                last = last.Next;
+            }
+            return last;
+        }
+        public ListElements FirstElement { get; set; }
+        public TimeQueue(ListElements a_first = null)
+        {
+            FirstElement = a_first;
+        }
+
+        public void Enqueue(Node a_newData)
+        {
+            ListElements temp = new ListElements(a_newData);
+            temp.Next = FirstElement;
+            FirstElement = temp;
+        }
+
+        public ListElements Dequeue()
+        {
+            ListElements oneBeforLast = null;
+            ListElements last = Last(FirstElement, ref oneBeforLast);
+            //make it the last Node
+            if (oneBeforLast != null && oneBeforLast.Next != null)
+            {
+                oneBeforLast.Next = null;
+            }
+            //or was only one or it is the last now!
+            else if (oneBeforLast == null || oneBeforLast.Next == null)
+            {
+                FirstElement = null;
+            }
+            return last;
+        }
+
+        public bool IsEmpty()
+        {
+            return FirstElement == null;
+        }
+
+        public Node First()
+        {
+            return FirstElement.Data;
+        }
+
+    }
+}
---
+++ TimedQueue_Ex/user_menue.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Xml.Linq;
+
+namespace timedQueue_Ex_
+{
+    class UserMenue
+    {
+        static public void Add(string a_message, int a_delayTime, ref TimeQueue a_queueMessage)
+        {
+            a_queueMessage.Enqueue(new Node(a_message, a_delayTime));
+        }
+
+        static public void Show(Node a_data)
+        {
+            int delayMilliseconds = a_data.DelayTime();
+            System.Threading.Thread.Sleep(delayMilliseconds);
+            Console.WriteLine(a_data.Message());
+        }
+
+
+
+        static string Exit()
+        {
+            return "exit";
+        }
+
+    }
+    class Test
+    {
+        static public void Main()
+        {
+            TimeQueue timeQAueue = new TimeQueue();
+            Console.WriteLine("welcome to queue message");
+            while (true) {
+                int choice = 0;
+                Console.WriteLine("");
+                Console.WriteLine("");
+                Console.WriteLine("1. to Add messaage");
+                Console.WriteLine("2. to show message");
+                Console.WriteLine("3. to Exit program");
+                string input = Console.ReadLine();
+                if (!int.TryParse(input, out choice))
+                {
+                    Console.WriteLine("Invalid input. Please enter a valid number.");
+                    continue;
+                }
+                else if (choice == 1)
+                {
+                    Console.WriteLine("insert your message");
+                    string message = Console.ReadLine();
+                    UserMenue.Add(message, 10, ref timeQAueue);
+
+                }
+                else if (choice == 2)
+                {
+                    UserMenue.Show(timeQAueue.Dequeue().Data);
+                }
+                else if (choice == 3)
+                {
+                    return;
+                }
+            }
+        }
+    }
+}
